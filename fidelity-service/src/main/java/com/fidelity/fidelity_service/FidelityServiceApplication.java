d05332515c4a845b64704e6d60ed4ce75e9dcddf
lexmarcos <marcosuel.vieira@hotmail.com>
2024-12-09 15:12:23 -0300

    init project

---
+++ fidelity-service/src/main/java/com/fidelity/fidelity_service/FidelityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fidelity.fidelity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelityServiceApplication.class, args);
+	}
+
+}
---
+++ fidelity-service/src/test/java/com/fidelity/fidelity_service/FidelityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelity.fidelity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
