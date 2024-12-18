680cca139999bc382989b25e677804bc6d89d1d1
lexmarcos <marcosuel.vieira@hotmail.com>
2024-12-17 21:54:17 -0300

    fix(feat-13): fix the language of product utils

--- ecommerce-service/src/main/java/com/ecommerce/ecommerce_service/utils/ProductUtils.java
+++ ecommerce-service/src/main/java/com/ecommerce/ecommerce_service/utils/ProductUtils.java
@@ -7,16 +7,16 @@ import com.ecommerce.ecommerce_service.model.Product;
 public class ProductUtils {
   public static void printProductNames(List<Product> products) {
     if (products == null || products.isEmpty()) {
-      System.out.println("A lista de produtos está vazia ou é nula.");
+      System.out.println("The product list is empty or null.");
       return;
     }
 
-    System.out.println("Nomes dos produtos:");
+    System.out.println("Product names:");
     for (Product product : products) {
       if (product == null) {
-        System.out.println("Produto nulo encontrado.");
+        System.out.println("Null product found.");
       } else if (product.getName() == null) {
-        System.out.println("Produto com nome nulo encontrado. ID: " + product.getId());
+        System.out.println("Product with null name found. ID: " + product.getId());
       } else {
         System.out.println(product.getName());
       }
