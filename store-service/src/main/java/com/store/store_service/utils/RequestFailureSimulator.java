188dee10aabb25e2106b2fc35b93add858cd9a38
Thiago Cordeiro <thiagoty52@gmail.com>
2024-12-19 14:10:18 -0300

    feat-18: add atomic boolean class and create new functions to handle conditions

--- store-service/src/main/java/com/store/store_service/utils/RequestFailureSimulator.java
+++ store-service/src/main/java/com/store/store_service/utils/RequestFailureSimulator.java
@@ -1,29 +1,42 @@
 package com.store.store_service.utils;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;;
 
 public class RequestFailureSimulator {
-  private boolean inErrorState = false;
+  private AtomicBoolean inErrorState = new AtomicBoolean(false);
   private long lastErrorTimestamp = 0;
   private static final long ERROR_DURATION_MS = 5000;
   private final Random random = new Random();
 
+  private boolean isWithinErrorDuration(long currentTime) {
+    return (currentTime - lastErrorTimestamp) < ERROR_DURATION_MS;
+  }
+
+  private boolean shouldEnterErrorState(double probability) {
+    return random.nextDouble() < probability;
+  }
+
+  private synchronized void enterErrorState(long currentTime) {
+    inErrorState.set(true);
+    lastErrorTimestamp = currentTime;
+  }
+
   public boolean shouldFail(double probability) {
     long currentTime = System.currentTimeMillis();
 
-    if (inErrorState && (currentTime - lastErrorTimestamp) < ERROR_DURATION_MS) {
+    if (inErrorState.get() && isWithinErrorDuration(currentTime)) {
       return true;
     }
 
-    if (inErrorState && (currentTime - lastErrorTimestamp) >= ERROR_DURATION_MS) {
-      inErrorState = false;
+    if (inErrorState.get() && !isWithinErrorDuration(currentTime)) {
+      inErrorState.set(false);
     }
 
-    if (!inErrorState && random.nextDouble() < probability) {
-      inErrorState = true;
-      lastErrorTimestamp = currentTime;
+    if (!inErrorState.get() && shouldEnterErrorState(probability)) {
+      enterErrorState(currentTime);
     }
 
-    return inErrorState;
+    return inErrorState.get();
   }
 }
